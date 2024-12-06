b2230a6ed71ea945ce72b504100cf745bb2cd959
Bechir Ferjani <bechir_ferjani@yahoo.fr>
2024-12-06 22:42:55 +0100

    first commit

---
+++ lib/Screens/login_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+import 'signup_page.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+  final FocusNode _usernameFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/Sotacib.png',
+                  height: 200,
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  focusNode: _usernameFocusNode,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                        ),
+                        const Text('Se souvenir de moi'),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        // Handle forgot password logic here
+                      },
+                      child: const Text('Mot de passe oublié ?'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle login logic here
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MainPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50), // Stretched button
+                    backgroundColor: Colors.blue, // Blue color
+                  ),
+                  child: const Text(
+                    'Se connecter',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Vous n\'avez pas de compte ?'),
+                    TextButton(
+                      child: const Text('S\'inscrire'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignupPage()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 100),
+                Image.asset(
+                  'assets/images/Molins_Logo.png',
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
